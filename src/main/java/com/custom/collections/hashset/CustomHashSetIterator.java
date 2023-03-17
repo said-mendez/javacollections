5bac748bad4788948eb01d0cf7de7269ba932b0f
Said Mendez <eduardo.said.mndz@gmail.com>
2023-03-17 09:09:52 -0600

    add hashset iterator

---
+++ src/main/java/com/custom/collections/hashset/CustomHashSetIterator.java
@@ -0,0 +1,41 @@
+package com.custom.collections.hashset;
+
+import com.custom.collections.CustomListIterator;
+import com.custom.collections.linkedlist.CustomLinkedList;
+
+public class CustomHashSetIterator<E> implements CustomListIterator<E> {
+    private int arrayCurrentIndex = 0, linkedListElementIndex = 0, numberOfElementsReturned = 0;
+    private final int numberOfElements;
+    private final CustomLinkedList<E>[] buckets;
+
+    public CustomHashSetIterator(CustomLinkedList<E>[] buckets, int size) {
+        this.buckets = buckets;
+        numberOfElements = size;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return numberOfElementsReturned < numberOfElements;
+    }
+
+    @Override
+    public E next() {
+        E nextElement = null;
+
+        if (buckets[arrayCurrentIndex] != null) {
+            CustomLinkedList<E> linkedList = buckets[arrayCurrentIndex];
+
+            nextElement = linkedList.get(linkedListElementIndex);
+            linkedListElementIndex++;
+            numberOfElementsReturned++;
+
+            // When we reach the last element in the list, then we can pass to the next index in the buckets array
+            if (linkedListElementIndex == linkedList.size()) {
+                arrayCurrentIndex++;
+                // Reset linkedListElementIndex as we will start with a new list
+                linkedListElementIndex = 0;
+            }
+        }
+        return nextElement;
+    }
+}
